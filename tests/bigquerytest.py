0838502d3aa4548966c8e8bc1258bb47b7f81f65
Roy Krishnan <60521440+roykrishnan@users.noreply.github.com>
2024-12-02 12:57:06 -0500

    Update bigquerytest.py

--- tests/bigquerytest.py
+++ tests/bigquerytest.py
@@ -1,7 +1,7 @@
 import os
 from google.cloud import bigquery
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/rohitkrishnan/Desktop/kmedved/api_keys/pristine-nebula-408601-e0808f746e2e.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'DONT LEAK THIS'
 client = bigquery.Client()
 
 query = "SELECT CURRENT_TIMESTAMP()"
